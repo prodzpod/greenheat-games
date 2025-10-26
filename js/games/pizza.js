6b2a5f235e94801ce4a401d0c9440ea0a3d22aca
prodzpod <product0339@gmail.com>
2025-10-26 17:31:46 +0900

    feat: pizza, spam

---
+++ js/games/pizza.js
@@ -0,0 +1,38 @@
+(() => {
+let LEADERBOARD;
+let LEADERBOARD_ORDER;
+ON_START["pizza"] = () => {
+    LEADERBOARD = {};
+    LEADERBOARD_ORDER = [];
+}
+ON_MESSAGE["pizza"] = (message) => {
+    const BUTTON = { x: WINDOW_SIZE.width - 80, y: 80, r: 64 };
+    if (message.type !== "click") return;
+    if (Math.hypot(message.x - BUTTON.x, message.y - BUTTON.y) > BUTTON.r) return;
+    LEADERBOARD[message.id] ??= { name: message.name === "" ? `Anonymous ${message.id}` : message.name, color: message.color, point: 0 };
+    LEADERBOARD[message.id].point += 1;
+    LEADERBOARD_ORDER = Object.keys(LEADERBOARD).sort((a, b) => LEADERBOARD[b].point - LEADERBOARD[a].point);
+}
+ON_DRAW["pizza"] = (canvas, ctx) => {
+    const BUTTON = { x: WINDOW_SIZE.width - 80, y: 80, r: 64 };
+    let idx = 1;
+    for (const id of LEADERBOARD_ORDER) {
+        const entry = LEADERBOARD[id];
+        ctx.font = "24px sans-serif"; 
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
+        ctx.fillStyle = entry.color;
+        ctx.fillText(`${idx}: ${entry.name} (${entry.point})`, 16, 32 * idx - 16);
+        idx += 1;
+    }
+    ctx.fillStyle = COLORS.foreground;
+    ctx.beginPath();
+    ctx.arc(BUTTON.x, BUTTON.y, BUTTON.r, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.font = "24px sans-serif"; 
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = COLORS.text;
+    ctx.fillText("Free Pizza", BUTTON.x, BUTTON.y);
+}
+})(); // ftr this is how you namespace in JS
---
+++ js/games/spam.js
@@ -0,0 +1,24 @@
+(() => {
+let POINT
+ON_START["spam"] = () => {
+    POINT = 0;
+}
+ON_MESSAGE["spam"] = (message) => {
+    if (message.type !== "click") return;
+    POINT += 1;
+}
+ON_DRAW["spam"] = (canvas, ctx) => {
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    ctx.beginPath();
+    ctx.rect(0, 0, WINDOW_SIZE.width, WINDOW_SIZE.height);
+    ctx.fill();
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.font = "48px sans-serif"; 
+    ctx.textBaseline = "bottom";
+    ctx.fillText("SPAM THE SCREEN !!", WINDOW_SIZE.width / 2, WINDOW_SIZE.height / 2 - 8);
+    ctx.font = "24px sans-serif"; 
+    ctx.textBaseline = "top";
+    ctx.fillText(`Counter: ${POINT}`, WINDOW_SIZE.width / 2, WINDOW_SIZE.height / 2 + 8);
+}
+})(); // ftr this is how you namespace in JS
